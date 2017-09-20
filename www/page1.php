075aacdcc4fd82302d7e389e56e0e5a95d42a955
renanrmoraes <rmoraes@f9c.com.br>
2017-09-20 15:18:35 -0300

    Create page1.php

---
+++ www/page1.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>F9C - OPENSHIFT LAB</title>
+ </head>
+ <body>
+        <img src="http://www.f9c.com.br/imagens/logo.png" alt="" title="F9C"><br><br>
+        <b>Container Hostname: </b>
+        <?php
+                echo gethostname();
+        ?>
+ </body>
+</html>
