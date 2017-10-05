9d7dde40dfc69aeb5eefc0bd7beffeb3fbbb00c2
renanrmoraes <rmoraes@f9c.com.br>
2017-10-05 16:59:54 -0300

    Update index.php

--- www/index.php
+++ www/index.php
@@ -15,7 +15,7 @@
 	</style>
 </head>
 <body>
-	<img id="logo" src="http://www.f9c.com.br/imagens/logo.png" />
+	<img id="logo" src="https://blog.openshift.com/wp-content/uploads/Logotype_RH_OpenShiftContainerPlatform_wLogo_CMYK_Black-768x198.jpg" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
